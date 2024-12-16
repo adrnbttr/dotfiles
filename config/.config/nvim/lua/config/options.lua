3111f1e259adc4990e570cb57627877cb33c5776
Adrien Bouttier <adrnbttr@gmail.com>
2024-12-16 20:59:15 +0100

    Update config

--- config/.config/nvim/lua/config/options.lua
+++ config/.config/nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+
+-- Configuration globale par défaut
+vim.opt.expandtab = true -- Utiliser des espaces au lieu des tabulations
+vim.opt.tabstop = 4 -- Largeur d'une tabulation
+vim.opt.shiftwidth = 4 -- Largeur de l'indentation
+vim.opt.softtabstop = 4 -- Largeur des tabulations en mode insertion
