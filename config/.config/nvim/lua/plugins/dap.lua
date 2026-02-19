6a7d171cc5de9af9c99aab6b6f4b042bfc5fe879
Adrien Bouttier <adrnbttr@gmail.com>
2026-02-19 23:19:55 +0100

    fix(nvim): migrate mason.nvim from williamboman to mason-org namespace

--- config/.config/nvim/lua/plugins/dap.lua
+++ config/.config/nvim/lua/plugins/dap.lua
@@ -143,7 +143,7 @@ return {
   
   -- Mason integration for automatic installation of debug adapters
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, { "js-debug-adapter" })
