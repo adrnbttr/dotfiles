76d44cc8185749b08b982c4a31042eeb5852afbd
Adrien Bouttier <adrnbttr@gmail.com>
2026-02-20 12:04:06 +0100

    fix(nvim): enable undofile for undotree persistence

--- config/.config/nvim/lua/config/options.lua
+++ config/.config/nvim/lua/config/options.lua
@@ -9,3 +9,7 @@ vim.opt.tabstop = 4 -- Largeur d'une tabulation
 vim.opt.shiftwidth = 4 -- Largeur de l'indentation
 vim.opt.softtabstop = 4 -- Largeur des tabulations en mode insertion
 vim.g.autoformat = false
+
+-- Enable persistent undo for undotree
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
